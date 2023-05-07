eecb023554e9b41f0379aaccf9fbbd321b01f3a2
Nemurs <kevinoctaviomejia@gmail.com>
2023-05-06 17:18:21 -0700

    DRYing up models/album.py

--- app/models/album.py
+++ app/models/album.py
@@ -4,11 +4,12 @@ from .db import db, environment, SCHEMA, UniqueConstraint
 class Album(db.Model):
     __tablename__ = 'albums'
 
+    __table_args__ = tuple([UniqueConstraint('title', 'artist_id', name='_title_artist_uc')])
+
     if environment == "production":
-        __table_args__ = (UniqueConstraint('title', 'artist_id', name='_title_artist_uc'),
+        #add schema if env is prod
+        __table_args__ = ( __table_args__[0],
                           {'schema': SCHEMA})
-    else:
-        __table_args__ = (UniqueConstraint('title', 'artist_id', name='_title_artist_uc'),)
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
@@ -17,8 +18,6 @@ class Album(db.Model):
     genre = db.Column(db.String(20), nullable=False)
     year = db.Column(db.Integer, nullable=False)
     artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
-
-    __table_args__ = (UniqueConstraint('title', 'artist_id', name='_title_artist_uc'),)
 
     albums_artists_relationship = db.relationship('Artist', back_populates='artists_albums_relationship')
     albums_songs_relationship = db.relationship('Song', back_populates='songs_albums_relationship')
