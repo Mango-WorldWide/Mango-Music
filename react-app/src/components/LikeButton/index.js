e6a2fd23e5668ed12f9cfa6f1f4e03629cd56527
Nemurs <kevinoctaviomejia@gmail.com>
2023-05-08 13:44:00 -0700

    cleaned up component code

--- react-app/src/components/LikeButton/index.js
+++ react-app/src/components/LikeButton/index.js
@@ -1,22 +1,10 @@
 import { useDispatch, useSelector } from "react-redux"
-import { loadLikesThunk,  deleteLikeThunk, createLikeThunk} from "../../store/like"
-import { useEffect } from "react"
-import { useHistory, useParams } from "react-router-dom"
-import { loadOneAlbumThunk } from "../../store/album"
+import { deleteLikeThunk, createLikeThunk} from "../../store/like"
+import { useHistory } from "react-router-dom"
 
 const LikeButton = ({song, isLiked}) =>  {
     const dispatch = useDispatch()
-    const history = useHistory()
     const likes = useSelector(state => Object.values(state.likes))
-
-    // const { albumId } = useParams()
-    // console.log(Object.values(album),'album state checking')
-    // useEffect(()=>{
-    //     console.log('inside album by id', albumId)
-    //     dispatch(loadOneAlbumThunk(albumId))
-    // },[dispatch])
-    // console.log(album.id)
-    // if (!album["Songs"]) return null
 
     const handleClick = async (e) => {
         e.preventDefault()
@@ -28,8 +16,6 @@ const LikeButton = ({song, isLiked}) =>  {
         else{
             await dispatch(createLikeThunk({"song_id": song.id}))
         }
-
-        // history.push(`/albums`)
     }
     return(
         <div>
