c0bfae95f8bdb9054331eddb9eb212e5ad3d5c61
dorianinc <dorianrdba@gmail.com>
2023-05-09 10:48:27 -0700

    have most of a working music player set up

--- app/models/song.py
+++ app/models/song.py
@@ -29,7 +29,9 @@ class Song(db.Model):
             'mp3': self.mp3,
             'lyrics': self.lyrics,
             'artist_id': self.artist_id,
-            'album_id': self.album_id
+            'album_id': self.album_id,
+            'artist': self.songs_artists_relationship.to_dict(),
+            'album': self.songs_albums_relationship.to_dict(), 
         }
 
     def to_dict_no_item(self):
--- react-app/src/components/AudioPlayer/index.js
+++ react-app/src/components/AudioPlayer/index.js
@@ -1,40 +1,45 @@
-import { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { loadSongsThunk } from "../../store/song"
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadSongsThunk } from "../../store/song";
 // import new_song from '../static/Music/Bad Bunny - Un Verano Sin Ti/03. Me Porto Bonito.mp3'
 const AudioPlayer = () => {
-    const [queueIndex, setQueueIndex] = useState(0)
-    const dispatch = useDispatch()
-    const getSongs = useSelector(state => state.songs)
-    const songs = Object.values(getSongs)
-    console.log(songs[0])
-    const song = songs.map(x => x["mp3"] )
-    useEffect(() => {
-        dispatch(loadSongsThunk())
-    },[dispatch])
-    if (!getSongs) return null
+  const [queueIndex, setQueueIndex] = useState(0);
+  const dispatch = useDispatch();
+  const getSongs = useSelector((state) => state.songs);
+  const songs = Object.values(getSongs);
+  console.log("songs 👉", songs);
+  console.log(songs[0]);
+  const song = songs.map((x) => x["mp3"]);
 
-    const goForward = () => {
-        if (queueIndex < songs.length - 1){
-          setQueueIndex(prev => prev + 1)
-        }
-      }
+  useEffect(() => {
+    dispatch(loadSongsThunk());
+  }, [dispatch]);
+  if (!getSongs) return null;
 
-      const goBack = () => {
-        if (queueIndex > 0 ){
-          setQueueIndex(prev => prev - 1)
-        }
-      }
+  const goForward = () => {
+    if (queueIndex < songs.length - 1) {
+      setQueueIndex((prev) => prev + 1);
+    }
+  };
 
-    return (
-        <div>
-
-            <audio src={song[queueIndex]} controls autoPlay={true} loop={true}></audio>
-            <button onClick={goBack}>back</button>
-            <button onClick={goForward}>forward</button>
-        </div>
-    )
-}
-export default AudioPlayer
-{/* <audio src="/static/media/01. Moscow Mule.8f5a3201.mp3" controls=""></audio> */}
-// http://localhost:3000/static/media/03. Me Porto Bonito.99dba593.mp3
+  const goBack = () => {
+    if (queueIndex > 0) {
+      setQueueIndex((prev) => prev - 1);
+    }
+  };
+  if(!songs.length) return null
+  console.log("=====>", songs[queueIndex])
+  return (
+    <div>
+      <img className="musicCover" src={songs[queueIndex].album.cover} />
+      <div>
+        <h3 className="title">{songs[queueIndex].title}</h3>
+        <p className="subTitle">{songs[queueIndex].artist.name}</p>
+      </div>
+      <audio src={song[queueIndex]} controls autoPlay={true} loop={true}></audio>
+      <button onClick={goBack}>back</button>
+      <button onClick={goForward}>forward</button>
+    </div>
+  );
+};
+export default AudioPlayer;
