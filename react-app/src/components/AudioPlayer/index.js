19dc2704148101d61a7233278dd23526790ea421
Nemurs <kevinoctaviomejia@gmail.com>
2023-05-10 10:56:18 -0700

    added basic volume bar and mute button

--- react-app/src/components/AudioPlayer/index.js
+++ react-app/src/components/AudioPlayer/index.js
@@ -1,18 +1,26 @@
 import { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loadSongsThunk } from "../../store/song";
-import "./AudioPlayerIndex.css"
-// import new_song from '../static/Music/Bad Bunny - Un Verano Sin Ti/03. Me Porto Bonito.mp3'
+import "./AudioPlayerIndex.css";
+
+/*FOR TESTING */
+import song1 from "../../Music/Kanye West - Graduation (2007)/01 Good Morning.mp3";
+import song2 from "../../Music/Kanye West - Graduation (2007)/02 Champion.mp3";
+import song3 from "../../Music/Kanye West - Graduation (2007)/03 Stronger.mp3";
+const song = [song1, song2, song3];
+/* */
 const AudioPlayer = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [IsLooping, setIsLooping] = useState(false);
+  const [unmuteVolume, setUnmuteVolume] = useState(false);
+  const [volume, setVolume] = useState(50);
   const [queueIndex, setQueueIndex] = useState(0);
   const dispatch = useDispatch();
   const getSongs = useSelector((state) => state.songs);
   const songs = Object.values(getSongs);
   console.log("songs 👉", songs);
-  console.log(songs[0]);
-  const song = songs.map((x) => x["mp3"]);
+  // console.log(songs[0]);
+  // const song = songs.map((x) => x["mp3"]);
 
   const audioPlayer = useRef();
 
@@ -29,6 +37,13 @@ const AudioPlayer = () => {
       }
     }
   }, [isPlaying, audioPlayer, queueIndex]);
+
+  useEffect(() => {
+    if (audioPlayer && audioPlayer.current) {
+      audioPlayer.current.volume = volume / 100;
+      audioPlayer.current.muted = unmuteVolume;
+    }
+  }, [volume, audioPlayer, unmuteVolume]);
 
   if (!getSongs) return null;
 
@@ -74,7 +89,14 @@ const AudioPlayer = () => {
         <span>Progress Bar</span>
       </div>
       <div className="audio-player-volume-controls">
-        <span>Volume Bar</span>
+        <button onClick={() => setUnmuteVolume((prev) => !prev)}>{unmuteVolume?"Unmute":"Mute"}</button>
+        <input
+          type="range"
+          min={0}
+          max={100}
+          value={volume}
+          onChange={(e) => setVolume(e.target.value)}
+        />
       </div>
       <audio src={song[queueIndex]} ref={audioPlayer} loop={IsLooping} style={{ display: "hidden" }}></audio>
     </div>
