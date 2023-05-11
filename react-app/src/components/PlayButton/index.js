e3022e0b849de1dbac757dda4b1649feeb16da17
Alancittoo <echeniquealan@yahoo.com>
2023-05-11 01:56:19 -0400

    css files added

--- react-app/src/components/PlayButton/index.js
+++ react-app/src/components/PlayButton/index.js
@@ -26,15 +26,23 @@ const PlayButton = ({ songId, albumId }) => {
   const handleClick = () => {
     console.log('hey i clicked it')
 
+    if (isPlaying && currentSong === songIndex) {
+      setIsPlaying(false);
+    } else {
     setSongsArr(songs)
     setIsPlaying(true);
     setCurrentSong(songIndex)
-  };
+  }}
 
 
   return (
     <>
-       <button onClick={handleClick}>Play</button>
+       <p onClick={handleClick} className="play-pause-btn">
+       {isPlaying && currentSong === songIndex ? (
+      <i className="fa fa-pause" aria-hidden="true"></i>
+                        ) : (
+      <i class="fa fa-play" aria-hidden="true"></i>
+       )}</p>
       {/* <audio src={song.mp3} ref={audioPlayer}></audio> */}
     </>
   );
