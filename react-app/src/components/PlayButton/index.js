888bae5249e3ab101a41c3c53abd81526261ef41
Alan Nguyen <alanngn21@gmail.com>
2023-05-10 23:49:12 -0700

    fixed

--- react-app/src/components/PlayButton/index.js
+++ react-app/src/components/PlayButton/index.js
@@ -10,7 +10,7 @@ const PlayButton = ({ songId, songs }) => {
   const songIndex = songArrId.indexOf(songId)
   console.log(songId, ' song id inside play button')
   const handleClick = () => {
-    console.log('hey i clicked it')
+
 
     if (isPlaying && currentSong === songIndex) {
       setIsPlaying(false);
@@ -19,7 +19,7 @@ const PlayButton = ({ songId, songs }) => {
     setIsPlaying(true);
     setCurrentSong(songIndex)
   };
-
+  }
 
   return (
     <>
@@ -29,7 +29,7 @@ const PlayButton = ({ songId, songs }) => {
                         ) : (
       <i class="fa fa-play" aria-hidden="true"></i>
        )}</p>
-      {/* <audio src={song.mp3} ref={audioPlayer}></audio> */}
+
     </>
   );
 };
