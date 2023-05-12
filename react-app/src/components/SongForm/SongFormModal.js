fdac630f1a654b55deb5b6d990588fcfb613065a
Alancittoo <echeniquealan@yahoo.com>
2023-05-12 19:14:23 -0400

    added loading

--- react-app/src/components/SongForm/SongFormModal.js
+++ react-app/src/components/SongForm/SongFormModal.js
@@ -6,13 +6,7 @@ function SongFormModal({ isSending }) {
         <div className="modal">
           <div className="modal-content">
             <h2>File is Sending</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident perferendis
-              suscipit officia recusandae, eveniet quaerat assumenda id fugit, dignissimos maxime
-              non natus placeat illo iusto! Sapiente dolorum id maiores dolores? Illum pariatur
-              possimus quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt placeat
-              tempora vitae enim incidunt porro fuga ea.
-            </p>
+            <img src={process.env.PUBLIC_URL + '/loading.gif'}></img>
           </div>
         </div>
         </div>
