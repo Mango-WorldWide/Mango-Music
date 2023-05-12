3b1ab7eb268cb69c4224b9f0c51f93764a1f553b
dorianinc <dorianrdba@gmail.com>
2023-05-12 15:49:11 -0700

    added modal to add songsform

--- react-app/src/components/SongForm/index.js
+++ react-app/src/components/SongForm/index.js
@@ -1,44 +1,69 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addSongThunk } from "../../store/song";
-import "./SongForm.css"
+import SongFormModal from "./SongFormModal.js";
+import "./SongForm.css";
 
 const SongForm = ({ albumId }) => {
   const dispatch = useDispatch(); // so that we can redirect after the image upload is successful
   const [mp3, setMp3] = useState("");
   const [genre, setGenre] = useState("");
   const [title, setTitle] = useState("");
-  console.log(albumId, "song submitting");
+  const [isSending, setIsSending] = useState(false);
+
+  useEffect(() => {
+    setIsSending(false);
+  }, [title, genre, mp3]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("HANDLING SUBMIT OF SONG")
+    setIsSending(true);
+    console.log("HANDLING SUBMIT OF SONG");
     const formData = new FormData();
     formData.append("mp3", mp3);
     formData.append("title", title);
     formData.append("genre", genre);
     formData.append("album_id", albumId);
-    console.log("formData 👉", mp3, title, genre, albumId, formData);
-    await dispatch(addSongThunk(formData));
-    setMp3("");
+
+    const newSong = await dispatch(addSongThunk(formData));
+    e.target[2].value = null;
     setTitle("");
-    
+    setGenre("");
   };
 
   return (
     <>
-    <form onSubmit={handleSubmit} className="songForm" encType="multipart/form-data">
-      <label>
-        title
-        <input className="titleInput" type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-      </label>
-      <label>
-        genre
-        <input className="genreInput"  type="text" value={genre} onChange={(e) => setGenre(e.target.value)} />
-      </label>
-      <input className="fileInput" type="file" accept="audio/*" onChange={(e) => setMp3(e.target.files[0])} />
-      <button className="orangeButton songSubmit"type="submit">Submit</button>
-    </form>
-    <hr/>
+      <SongFormModal isSending={isSending} />
+      <form onSubmit={handleSubmit} className="songForm" encType="multipart/form-data">
+        <label>
+          title
+          <input
+            className="titleInput"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </label>
+        <label>
+          genre
+          <input
+            className="genreInput"
+            type="text"
+            value={genre}
+            onChange={(e) => setGenre(e.target.value)}
+          />
+        </label>
+        <input
+          className="fileInput"
+          type="file"
+          accept="audio/*"
+          onChange={(e) => setMp3(e.target.files[0])}
+        />
+        <button className="orangeButton songSubmit" disabled={isSending} type="submit">
+          Submit
+        </button>
+      </form>
+      <hr />
     </>
   );
 };
