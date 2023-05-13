f76e9d285a4d761f8e26e4c8b1a9859fdb9e6cc6
dorianinc <dorianrdba@gmail.com>
2023-05-13 07:18:36 -0700

    threw a little not in there for future devs

--- app/api/songs_routes.py
+++ app/api/songs_routes.py
@@ -116,10 +116,11 @@ def delete_song(songId):
     song = Song.query.get(songId)
     artist = current_user.to_dict()
     deleted_song = song.to_dict()
+    poke = True
     print("current user artists id 👉👉👉", artist["artist_id"])
     print("current songs artists id 👉👉👉", song.to_dict_no_item(includeMP3 = True)["artist_id"])
+    print("original conditional  VVVVVVVVVVVVVV")
     print(" is this the owner? 👉👉👉", artist["artist_id"] == song.to_dict_no_item(includeMP3 = True)["artist_id"])
-    poke = True
     if poke:
         # print(" song url 👉👉👉", song.to_dict_no_item(includeMP3 = True)["mp3"])
         test = remove_file_from_s3('http://mango-music.s3.amazonaws.com/04b6550c621641c8bd3f1a14418e90bd.mp3')
