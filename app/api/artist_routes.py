ce94316704b80baf3852ef6931824279cccc9a32
dorianinc <dorianrdba@gmail.com>
2023-05-13 05:36:41 -0700

    added get artists_albums route

--- app/api/artist_routes.py
+++ app/api/artist_routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, make_response
+from flask_login import login_required, current_user
 from app.models import Artist, db
 
 artist_routes = Blueprint('artist', __name__)
@@ -17,5 +18,24 @@ def validation_errors_to_error_messages(validation_errors):
 
 @artist_routes.route('/<int:artistId>')
 def get_artist(artistId):
+    """get specific artist"""
     artist = Artist.query.get(artistId)
     return artist.to_dict()
+
+
+@artist_routes.route("/albums")
+@login_required
+def get_artists_albums():
+    """get artist's albums"""
+    user = current_user.to_dict()
+    artist_id = user["artist_id"]
+
+    if artist_id:
+        number_list = [ x for x in range(20) if x % 2 == 0]
+        artist = Artist.query.get(artist_id)
+        albums = [album for album in (artist.to_dict())["albums"]]
+        return albums
+    else:
+        error = make_response("Must be an artist to access this")
+        error.status_code = 404
+        return error
